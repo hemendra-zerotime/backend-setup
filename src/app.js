9b54267f1a9731fe0e79204d3d8763e90f2ccfee
hemendra-zerotime <hemendra.zerotimesolutions@gmail.com>
2025-08-05 11:48:28 +0530

    correct spell

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cors from "cors"
 const app = express()
 app.use(cookieParser())
 app.use(cors({origin:process.env.CORS_ORIGIN,
-    cred2entials:true,
+    credentials:true,
 }))
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
