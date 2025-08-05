d70228d0db5b8b7e5e39d5490c30e7a8a89a895b
hemendra-zerotime <hemendra.zerotimesolutions@gmail.com>
2025-08-05 10:49:10 +0530

    request accept setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import dotenv from "dotenv"
 import dbConnect from "./db/db.js"
+import { app } from "./app.js"
 dotenv.config({path:'./env'})
+const port = process.env.PORT || 8000
 
 
-dbConnect()
+dbConnect().then((async () => {
+    app.on("error",(error)=>{
+        console.log(`Error: ${error}`)
+        throw error
+    })
+    app.listen(port,()=>{
+        console.log(`server is listening to port: ${port}`) 
+    })
+})).catch((error)=>{
+    console.log(error)
+})
